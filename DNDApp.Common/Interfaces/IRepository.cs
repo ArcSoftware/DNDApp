0bf9b54977675ada61823ce675d1b629daf9f677
Jake Tepper <jaketepper@gmail.com>
2019-01-10 11:52:52 -0500

    first commit

---
+++ DNDApp.Common/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DNDApp.Common.Interfaces
+{
+    public interface IRepository
+    {
+        IQueryable<T> GetItems<T>(Expression<Func<T, bool>> predicate) where T : class;
+
+        void Add<T>(T entity) where T : class;
+
+        void Update<T>(T entity) where T : class;
+
+        void Delete<T>(T entity) where T : class; 
+    }
+}
---
+++ DNDApp.Data/Repository/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using DNDApp.Common.Interfaces;
+
+namespace DNDApp.Data.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        public IQueryable<T> GetItems<T>(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
